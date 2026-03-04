4dab0f00a30342bf3f6bd22571bb87d364fbc374
Vrynd <vryndhika@gmail.com>
2026-03-04 18:34:04 +0700

    Mengatur role dan middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,16 +8,17 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+   /**
+    * Seed the application's database.
+    */
+   public function run(): void
+   {
+      // Create Admin
+      User::factory()->create([
+         'name' => 'Admin Sureka',
+         'email' => 'admin@sureka.com',
+         'password' => bcrypt('password'), // Password eksplisit
+         'role' => 'admin',
+      ]);
+   }
 }
